bc029e2333e55677e750eddc53d42b963eb107c6
malqattan4 <106008076+malqattan4@users.noreply.github.com>
2022-07-20 12:06:07 +0300

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class Order {
+    public String name;
+    public double total;
+    public boolean ready;
+    ArrayList<Item> items= new ArrayList<Item>();
+
+    
+}
+
+
+
